6e6174aada7cde335036a7a96b8f774112aba35c
Аш <achanonymous@duck.com>
2024-10-10 13:22:34 +0300

    Add files via upload

---
+++ baseinstallubuntu.sh
@@ -0,0 +1,25 @@
+apt install neofetch
+echo "тестируем neofetch"
+neofetch
+echo "neofetch работает! устанавливаем firefox и пиритим бубунту про"
+apt install firefox
+echo "тестируем firefox"
+firefox
+echo "firefox работает! устанавливаем всякие прихолюхи по типу cmatrix и cowsay"
+apt install cowsay
+cowsay тест
+echo "если вы увидили текст который говорит корова значит cowsay установился"
+echo "устанавливаем cmatrix"
+apt install cmatrix
+echo "cmatrix установлен!"
+echo "устанавливаем discord"
+apt install discord
+echo "тестируем discord"
+discord
+echo "discord установлен!"
+echo "устанавливаем Telegram"
+apt install telegram-desktop
+echo "Telegram Установлен!"
+echo "все базовые программы установлены!"
+echo "поддержать автора: https://boosty.to/achach/donate"
+echo "спасибо за использование программы!"
